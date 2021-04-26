1cb975e21e5eadb173697534940ff9b4d67757f6
Abdulwakil Jamiu <aremcx84@gmail.com>
2021-04-26 23:52:40 +0100

    Django blog CRUD

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='post')
+    body = models.TextField()
+    
+
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_details', args=[str(self.id)])
