a34d45d8427fe3db977c978a34320d90f44c22a6
Abdulwakil Jamiu <aremcx84@gmail.com>
2021-04-26 23:56:48 +0100

    Django framework blog CRUD

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'blog',
     'accounts',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,7 +127,8 @@ STATIC_URL = '/static/'
 #STATICFILES_DIRS = [str(BASE_DIR.joinpath('config/static/css'))] 
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('config/static'))]
 STATIC_ROOT = STATIC_ROOT = str(BASE_DIR.joinpath('config/staticfiles'))
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 #STATICFILES_DIRS = ["config/static"],
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
